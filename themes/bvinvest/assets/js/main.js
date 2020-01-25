3295d9a7515f90b9e6032fc07206a9382e9ffc70
shantoztech <45969365+shantoztech@users.noreply.github.com>
2020-01-25 13:56:27 +0600

    pages

--- themes/bvinvest/assets/js/main.js
+++ themes/bvinvest/assets/js/main.js
@@ -206,7 +206,11 @@ if( $('.hmGrdSlider').length ){
 }
 
 
-
+$('.slider-filter-bar > a').on('click', function(e){
+  e.preventDefault();
+  $(this).addClass('active');
+  $(this).siblings().removeClass('active');
+});
 
 /*
   Milon
